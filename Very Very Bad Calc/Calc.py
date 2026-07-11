9c42f2e60fe1a646b323a06161cdb6b7abf51b82
Artemis-UI <phillippaul.singer@gmail.com>
2026-07-11 14:23:34 +0200

    add: Buttons for +,-,*,/ and a funktion that changes the state of the Operators to true if they have been selected  deleted Intern.py cause it only complicated things

--- Very Very Bad Calc/Calc.py
+++ Very Very Bad Calc/Calc.py
@@ -1,17 +1,45 @@
 from tkinter import*
-from Intern import outvaria
+
+outvaria = "main"
+Plus = False
+Minus = False
+Divide = False
+Addition = False
+
+def Type():
+    Plus = True
+    return Plus
+def Type2():
+    Minus = True
+    return Minus
+def Type3():
+    Addition = True
+    return Addition
+def Type4():
+    Divide = True
+    return Divide
 
 
-def submit():
+def Calculate():
     Num1 = Int1.get()
     Num2 = Int2.get()
     Int1.delete(0,END)
     Int2.delete(0,END)
 
 
+
+
+
+    
+    
+
+    
+
+
 window = Tk()
 window2 = Tk()
 window3 = Tk()
+
 
 Output = Label(window,
                text=outvaria,
@@ -28,7 +56,7 @@ Int1 = Entry(window2,
              bg='BLACK',
              fg='white')
 
-Int2 = Entry(window3,
+Int2 = Entry(window2,
              font=('Arial',50),
              bg='BLACK',
              fg='white')
@@ -36,17 +64,45 @@ Int2 = Entry(window3,
 
 submit_button = Button(window,
                        text="Submit",
-                       command=submit,
+                       command=Calculate,
                        bg='BLACK',
                        fg='white')
+
+Plus_Button = Button(window3,
+                     text="+",
+                     command=Type,
+                     bg='BLACK',
+                     fg='WHITE',
+                     padx=20)
+
+Addition_Button = Button(window3,
+                     text="*",
+                     command=Type3,
+                     bg='BLACK',
+                     fg='WHITE',
+                     padx=20)
+
+Divide_Button = Button(window3,
+                     text="/",
+                     command=Type4,
+                     bg='BLACK',
+                     fg='WHITE',
+                     padx=20)
+
+Minus_Button = Button(window3,
+                      text="-",
+                      command=Type2,
+                      bg='BLACK',
+                      fg='WHITE',
+                      padx=20)
 
 window.geometry("200x100")
 window.title("Calculator")
 
-window2.geometry("200x100")
+window2.geometry("200x150")
 window2.title("Calculator")
 
-window3.geometry("200x100")
+window3.geometry("300x300")
 window3.title("Calculator")
 
 
@@ -57,6 +113,13 @@ Output.pack()
 Int1.pack()
 Int2.pack()
 submit_button.pack()
+
+
+Plus_Button.grid(row=0,column=0)
+Minus_Button.grid(row=0,column=1)
+Addition_Button.grid(row=0,column=2)
+Divide_Button.grid(row=0,column=3)
+
 window.mainloop()
 window2.mainloop()
 window3.mainloop()
