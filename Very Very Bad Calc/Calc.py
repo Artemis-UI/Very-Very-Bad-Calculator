9ee3da3c9dcbfaf2e9068805025a0e5a008c2ddd
Artemis-UI <phillippaul.singer@gmail.com>
2026-07-11 21:59:45 +0200

    added funktionality to the calculator

--- Very Very Bad Calc/Calc.py
+++ Very Very Bad Calc/Calc.py
@@ -1,33 +1,55 @@
-from tkinter import*
+from tkinter import *
 
-outvaria = "main"
 Plus = False
 Minus = False
 Divide = False
 Addition = False
 
+
 def Type():
+    global Plus, Minus, Divide, Addition
     Plus = True
-    return Plus
+    Minus = Divide = Addition = False
 def Type2():
+    global Plus, Minus, Divide, Addition
     Minus = True
-    return Minus
+    Plus = Divide = Addition = False
+
 def Type3():
+    global Plus, Minus, Divide, Addition
     Addition = True
-    return Addition
+    Minus = Divide = Plus = False
+
 def Type4():
+    global Plus, Minus, Divide, Addition
     Divide = True
-    return Divide
+    Minus = Addition = Plus = False
+
 
 
 def Calculate():
+    Num1 = 0
+    Num2 = 0
     Num1 = Int1.get()
     Num2 = Int2.get()
+    Num1 = int(Num1)
+    Num2 = int(Num2)
     Int1.delete(0,END)
     Int2.delete(0,END)
 
+    if Plus == True:
+        outvaria = Num1 + Num2
+    elif Minus == True:
+        outvaria = Num1 - Num2
+    elif Addition == True:
+        outvaria = Num1 * Num2
+    elif Divide == True:
+        outvaria = Num1 / Num2
+    else:
+        outvaria = "Operator Missing"
+    Varoa.set(outvaria)
 
-
+    
 
 
     
@@ -41,14 +63,8 @@ window2 = Tk()
 window3 = Tk()
 
 
-Output = Label(window,
-               text=outvaria,
-               font=('Arial',20,'bold'),
-               fg='Blue',
-               bg='Gray',
-               relief=SUNKEN,
-               bd=4,
-               padx=8)
+Varoa = StringVar()
+Varoa.set("main")
 
 
 Int1 = Entry(window2,
@@ -109,10 +125,12 @@ window3.title("Calculator")
 icon = PhotoImage(file='Calc.png')
 window.iconphoto(True,icon)
 window.config(background="Gray")
-Output.pack()
 Int1.pack()
 Int2.pack()
 submit_button.pack()
+
+Output = Label(window, textvariable=Varoa, font=('Arial',14))
+Output.pack(pady=20)
 
 
 Plus_Button.grid(row=0,column=0)
